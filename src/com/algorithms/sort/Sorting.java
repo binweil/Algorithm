c7c6dc6c586411852457cff86bc549b125db0739
Binwei Liu (Lamy) <binweili@usc.edu>
2020-10-23 01:58:43 -0700

    Bubble Sort

--- src/com/algorithms/sort/Sorting.java
+++ src/com/algorithms/sort/Sorting.java
@@ -5,12 +5,32 @@ import java.util.*;
 public class Sorting {
 
     public static void main(String[] args) {
-        int size = 100;
+        int size = 1000;
         int[] arr = new int[size];
         Random random = new Random();
         for (int i = 0; i < size; i++) {
             arr[i] = random.nextInt(size);
         }
+        long startTime = System.currentTimeMillis();
+        bubbleSort(arr);
+        long duration = System.currentTimeMillis() - startTime;
         System.out.println("Arrays is: " + Arrays.toString(arr));
+        System.out.println("Sorting duration: " + duration);
+    }
+
+    /**
+     * O(n2)
+     * @param arr
+     */
+    private static void bubbleSort (int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
     }
 }
